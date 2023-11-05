42ad56730cf8fc68bea6fd5bfb6d59dbd4439c47
Cedric Berthet <berthetcedric@gmail.com>
2023-11-05 19:31:22 +0100

    life

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Recuperation des mots dans le fichier JSON :
+
 fetch("mots.json")
 .then(Response => Response.json())
 .then(data => {
@@ -6,13 +8,14 @@ fetch("mots.json")
     console.log(motRandom)
     afficheMot(motRandom)
 })
+// Selectionner un mots aléatoire :
 
 function motAleatoire() {
     const motRandom = Math.floor(Math.random() * mots.length);
     return mots[motRandom];
-
 };
 
+// Création <p> DOM + affichage du mot aléatoire :
 function afficheMot(mot){
  let footer = document.querySelector('footer');
  let paragraphe = document.createElement('p')
@@ -21,3 +24,17 @@ paragraphe.setAttribute('id', 'mot')
 let parentElement = footer.parentElement;
 parentElement.insertBefore(paragraphe, footer);
 };
+
+// Création <p> DOM + Affichage vie restante :
+
+function vie(){
+    let h1 = document.querySelector('h1');
+    let paragraphe = document.createElement('p');
+    paragraphe.textContent = life;
+    paragraphe.setAttribute('id', 'vies')
+    h1.appendChild(paragraphe);
+}
+vie();
+// Affichage nombre de vies restantes :
+
+let life = 5;
